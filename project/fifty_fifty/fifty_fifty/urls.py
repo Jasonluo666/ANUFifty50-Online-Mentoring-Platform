d347d33bcf7823a1b10b4a12ab182921b949072e
Nikita Bhatia <nikitabhatia@Nikitas-MBP.gateway>
2017-07-17 21:24:37 +1000

    Tried adding submission option to admin contact form but collectstatic doesn't work

--- project/fifty_fifty/fifty_fifty/urls.py
+++ project/fifty_fifty/fifty_fifty/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from webcore import views
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -39,3 +40,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
