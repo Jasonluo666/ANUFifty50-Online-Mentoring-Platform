910adece486d4409981eb0579747a31989f55a43
Nikita Bhatia <nikitabhatia@Nikitas-MBP.gateway>
2017-07-18 21:40:33 +1000

    Uploading Tyrus' code for content manager

--- project/fifty_fifty/fifty_fifty/settings.py
+++ project/fifty_fifty/fifty_fifty/settings.py
@@ -139,7 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'fifty_fifty/static'),)
 
 if DEBUG:
     MEDIA_URL = '/media/'
