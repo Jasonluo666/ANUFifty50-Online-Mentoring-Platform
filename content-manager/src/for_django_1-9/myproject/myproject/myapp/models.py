82f70348b642a9fa61c87e639c9ef099efa654ab
Nikita Bhatia <nikitabhatia@Nikitas-MBP.gateway>
2017-07-19 23:50:29 +1000

    Tried the method from stack overflow, the code throws a runtime error

--- content-manager/src/for_django_1-9/myproject/myproject/myapp/models.py
+++ content-manager/src/for_django_1-9/myproject/myproject/myapp/models.py
@@ -7,16 +7,25 @@ from djchoices import DjangoChoices, ChoiceItem
 #    semester = models.CharField(max_length=16)
 
 class Week_Choices(DjangoChoices):
-    Week_1 = ChoiceItem(1, 'Week1')
-    Week_2 = ChoiceItem(2, 'Week2')
-    Week_3 = ChoiceItem(3, 'Week3')
-    Week_4 = ChoiceItem(4, 'Week4')
+    Week_1 = ChoiceItem(1, 'Week_1')
+    Week_2 = ChoiceItem(2, 'Week_2')
+    Week_3 = ChoiceItem(3, 'Week_3')
+    Week_4 = ChoiceItem(4, 'Week_4')
 
 class Role_Choices(DjangoChoices):
     Mentee = ChoiceItem(1, 'Mentee')
     Mentor = ChoiceItem(2, 'Mentor')
     Training = ChoiceItem(3, 'Training')
 
+def location(instance, filename):
+    if instance.Week == Week_1:
+        return 'documents/'+ Week_1 + '/' + 'Mentee' + filename
+    elif instance.Week == Week_2:
+        return 'documents/'+ Week_2 + '/' + 'Mentee' + filename
+    elif instance.Week == Week_3:
+        return 'documents/'+ Week_3 + '/' + 'Mentee' + filename
+    elif instance.Week == Week_4:
+        return 'documents/'+ Week_4 + '/' + 'Mentee' + filename
 
 class Document(models.Model):
 
@@ -46,7 +55,10 @@ class Document(models.Model):
 
     Mentee_Mentor = models.CharField(max_length=25, choices=Week_Choices, default=Mentee, blank=False)
     Week = models.CharField(max_length=10, choices=Role_Choices, default=Week_1, blank=False)
-    docfile = models.FileField(upload_to='documents/'+ 'mentee' + '/' + 'Mentee') # path may be malformed"""
+    #docfile = models.FileField(upload_to='documents/'+ 'mentee' + '/' + 'Mentee') # path may be malformed"""
+    docfile = models.FileField(upload_to=location)
+
+
 #class Document(models.Model):
     #docfile = models.FileField()
     #docfile = models.FileField(upload_to='documents/%Y/%m/%d')
