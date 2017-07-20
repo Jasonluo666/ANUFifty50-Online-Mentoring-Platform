2c7dc4f1431ac9db6d3c7bf2087ea7e5ad839d9e
Nikita Bhatia <nikitabhatia@Nikitas-MBP.gateway>
2017-07-20 20:59:55 +1000

    Fixed some type errors but uploading files still causes a runtime error

--- content-manager/src/for_django_1-9/myproject/myproject/myapp/models.py
+++ content-manager/src/for_django_1-9/myproject/myproject/myapp/models.py
@@ -1,41 +1,45 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from djchoices import DjangoChoices, ChoiceItem
+import os, sys
 #class Week(models.Model):
 
 #    week = models.IntegerField()
 #    semester = models.CharField(max_length=16)
 
 class Week_Choices(DjangoChoices):
-    Week_1 = ChoiceItem(1, 'Week_1')
-    Week_2 = ChoiceItem(2, 'Week_2')
-    Week_3 = ChoiceItem(3, 'Week_3')
-    Week_4 = ChoiceItem(4, 'Week_4')
+    Week_1 = ChoiceItem(1, 'Week1')
+    Week_2 = ChoiceItem(2, 'Week2')
+    Week_3 = ChoiceItem(3, 'Week3')
+    Week_4 = ChoiceItem(4, 'Week4')
 
 class Role_Choices(DjangoChoices):
     Mentee = ChoiceItem(1, 'Mentee')
     Mentor = ChoiceItem(2, 'Mentor')
     Training = ChoiceItem(3, 'Training')
-"""
+
 def location(instance, filename):
-    if instance.Week == Week_1:
+    if instance.Weekly_Choices == 'Week1':
         return 'documents/'+ Week_1 + '/' + 'Mentee' + filename
-    elif instance.Week == Week_2:
+    elif instance.Weekly_Choices == 'Week2':
         return 'documents/'+ Week_2 + '/' + 'Mentee' + filename
-    elif instance.Week == Week_3:
+    elif instance.Weekly_Choices == 'Week3':
         return 'documents/'+ Week_3 + '/' + 'Mentee' + filename
-    elif instance.Week == Week_4:
+    elif instance.Weekly_Choices == 'Week4':
         return 'documents/'+ Week_4 + '/' + 'Mentee' + filename
 """
 def location(instance, filename):
-    if Week.choices == Week_1:
+    if Week_Choices.choices == 'Week1':
         return os.path.join('documents', 'Week_1', 'Mentee', filename)
-    elif Week.choices == Week_2:
+    elif Week_Choices.choices == 'Week2':
         return os.path.join('documents', 'Week_2', 'Mentee', filename)
-    elif Week.choices == Week_3:
+    elif Week_Choices.choices == 'Week3':
         return os.path.join('documents', 'Week_3', 'Mentee', filename)
-    elif Week.choices == Week_4:
+    elif Week_Choices.choices == 'Week4':
         return os.path.join('documents', 'Week_4', 'Mentee', filename)
+    else:
+        return os.path.join('documents', filename)
+"""
 
 class Document(models.Model):
 
@@ -50,11 +54,11 @@ class Document(models.Model):
     Mentor = 'Mentee'
     Training = 'Training'
 
-    Week_Choices = (
+    Weekly_Choices = (
         (Week_1, 'Week_1'),
         (Week_2, 'Week_2'),
-        (Week_2, 'Week_3'),
-        (Week_2, 'Week_4')
+        (Week_3, 'Week_3'),
+        (Week_4, 'Week_4')
     )
 
     Role_Choices = (
@@ -63,8 +67,8 @@ class Document(models.Model):
         (Training, 'Training')
     )
 
-    Mentee_Mentor = models.CharField(max_length=25, choices=Week_Choices, default=Mentee, blank=False)
-    Week = models.CharField(max_length=10, choices=Role_Choices, default=Week_1, blank=False)
+    Mentee_Mentor = models.CharField(max_length=25, choices=Role_Choices, default=Mentee, blank=False)
+    Week = models.CharField(max_length=10, choices=Weekly_Choices, default=Week_1, blank=False)
     #docfile = models.FileField(upload_to='documents/'+ 'mentee' + '/' + 'Mentee') # path may be malformed"""
     docfile = models.FileField(upload_to=location)
 
