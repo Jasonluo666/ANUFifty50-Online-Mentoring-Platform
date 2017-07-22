f46694284517daabe650d94e8935fb7aada989a7
tyrus <tyrus.caldeira@yahoo.com>
2017-07-22 14:11:59 +1000

    Resource manager now works with a document structure

--- content-manager/src/for_django_1-9/myproject/myproject/myapp/models.py
+++ content-manager/src/for_django_1-9/myproject/myproject/myapp/models.py
@@ -1,67 +1,15 @@
+# -*- coding: utf-8 -*-
+import os
 from django.db import models
-from djchoices import DjangoChoices, ChoiceItem
-import os, sys
-
-def location(instance, filename):
-    if Week.instance == 'Week1':
-        return 'documents/' + Week1 + '/' + 'Mentee' + filename
-    elif Week.instance == 'Week2':
-        return 'documents/' + Week2 + '/' + 'Mentee' + filename
-    elif Week.instance == 'Week3':
-        return 'documents/' + Week3 + '/' + 'Mentee' + filename
-    elif Week.instance == 'Week4':
-        return 'documents/' + Week4 + '/' + 'Mentee' + filename
-        
-class Document(models.Model):
-    # Choices
-    class Week_Choices(DjangoChoices):
-        Week1 = ChoiceItem("Week1")
-        Week2 = ChoiceItem("Week2")
-        Week3 = ChoiceItem("Week3")
-        Week4 = ChoiceItem("Week4")
-
-    class Role_Choices(DjangoChoices):
-        Mentee = ChoiceItem("Mentee")
-        Mentor = ChoiceItem("Mentor")
-        Training = ChoiceItem("Training")
-
-    # Fields
-    Name = models.CharField(max_length=25, blank=True)
-    Week_Number = models.CharField(max_length=10, choices=Week_Choices.choices)
-    Mentee_or_Mentor = models.CharField(max_length=25, choices=Role_Choices.choices)
-    docfile = models.FileField(upload_to=location)
-
-
-
-"""
-class Week(DjangoChoices):
-    Week_1 = ChoiceItem(1, 'Week1')
-    Week_2 = ChoiceItem(2, 'Week2')
-    Week_3 = ChoiceItem(3, 'Week3')
-    Week_4 = ChoiceItem(4, 'Week4')
-
-class Role_Choices(DjangoChoices):
-    Mentee = ChoiceItem(1, 'Mentee')
-    Mentor = ChoiceItem(2, 'Mentor')
-    Training = ChoiceItem(3, 'Training')
-
-def location(instance, filename):
-    if Week.instance == 'Week1':
-        return 'documents/' + Week_1 + '/' + 'Mentee' + filename
-    elif Week.instance == 'Week2':
-        return 'documents/' + Week_2 + '/' + 'Mentee' + filename
-    elif Week.instance == 'Week3':
-        return 'documents/' + Week_3 + '/' + 'Mentee' + filename
-    elif Week.instance == 'Week4':
-        return 'documents/' + Week_4 + '/' + 'Mentee' + filename
 
 class Document(models.Model):
-    #week = models.ForeignKey('Week', ) # app name in ###, also is this optional?
-    Name = models.CharField(max_length=25, blank=True)
-    Week_1 = 'Week1'
-    Week_2 = 'Week2'
-    Week_3 = 'Week3'
-    Week_4 = 'Week4'
+
+    Filename = models.CharField(max_length=25, blank=True)
+
+    Week_1 = 'Week_1'
+    Week_2 = 'Week_2'
+    Week_3 = 'Week_3'
+    Week_4 = 'Week_4'
     Mentee = 'Mentee'
     Mentor = 'Mentee'
     Training = 'Training'
@@ -69,8 +17,8 @@ class Document(models.Model):
     Weekly_Choices = (
         (Week_1, 'Week_1'),
         (Week_2, 'Week_2'),
-        (Week_3, 'Week_3'),
-        (Week_4, 'Week_4')
+        (Week_2, 'Week_3'),
+        (Week_2, 'Week_4')
     )
 
     Role_Choices = (
@@ -79,38 +27,35 @@ class Document(models.Model):
         (Training, 'Training')
     )
 
-    Mentee_Mentor = models.CharField(max_length=25, choices=Role_Choices, default=Mentee, blank=False)
+    Role = models.CharField(max_length=25, choices=Role_Choices, default=Mentee, blank=False)
     Week = models.CharField(max_length=10, choices=Weekly_Choices, default=Week_1, blank=False)
-    #docfile = models.FileField(upload_to='documents/'+ 'mentee' + '/' + 'Mentee') # path may be malformed
-    docfile = models.FileField(upload_to=location)
-"""
 
+    def choices_location(instance, filename):
+        if instance.Week == 'Week_1' and instance.Role == 'Mentee':
+            return os.path.join('documents', 'Week_1', 'Mentee', filename)
+        elif instance.Week == 'Week_2' and instance.Role == 'Mentee':
+            return os.path.join('documents', 'Week_2', 'Mentee', filename)
+        elif instance.Week == 'Week_3' and instance.Role == 'Mentee':
+            return os.path.join('documents', 'Week_3', 'Mentee', filename)
+        elif instance.Week == 'Week_4' and instance.Role == 'Mentee':
+            return os.path.join('documents', 'Week_1', 'Mentee', filename)
+        elif instance.Week == 'Week_1' and instance.Role == 'Mentor':
+            return os.path.join('documents', 'Week_1', 'Mentor', filename)
+        elif instance.Week == 'Week_2' and instance.Role == 'Mentor':
+            return os.path.join('documents', 'Week_2', 'Mentor', filename)
+        elif instance.Week == 'Week_3' and instance.Role == 'Mentor':
+            return os.path.join('documents', 'Week_3', 'Mentor', filename)
+        elif instance.Week == 'Week_4' and instance.Role == 'Mentor':
+            return os.path.join('documents', 'Week_4', 'Mentor', filename)
+        elif instance.Week == 'Week_1' and instance.Role == 'Training':
+            return os.path.join('documents', 'Week_1', 'Training', filename)
+        elif instance.Week == 'Week_2' and instance.Role == 'Training':
+            return os.path.join('documents', 'Week_2', 'Training', filename)
+        elif instance.Week == 'Week_3' and instance.Role == 'Training':
+            return os.path.join('documents', 'Week_3', 'Training', filename)
+        elif instance.Week == 'Week_4' and instance.Role == 'Training':
+            return os.path.join('documents', 'Week_4', 'Training', filename)
+        else :
+            return os.path.join(filename)
 
-#class Document(models.Model):
-    #docfile = models.FileField()
-    #docfile = models.FileField(upload_to='documents/%Y/%m/%d')
-"""
-    name = models.CharField(max_length=25, blank=True)
-    Week_1 = 'W1'
-    Week_2 = 'W2'
-    Week_3 = 'W3'
-    Week_4 = 'W4'
-    Weekly_Content_CHOICES = (
-        (Week_1, 'Week1'),
-        (Week_2, 'Week2'),
-        (Week_3, 'Week3'),
-        (Week_4, 'Week4'),
-    )
-    Folder = models.CharField(
-        max_length=2,
-        choices=Weekly_Content_CHOICES,
-        default=Week_1,
-    )
-    if Weekly_Content_CHOICES == 'Week1'
-        docfile.upload_to = 'documents/Week1/'
-    elif Weekly_Content_CHOICES == 'Week2'
-        docfile.upload_to = 'documents/Week2/'
-    elif Weekly_Content_CHOICES == 'Week3'
-        docfile.upload_to = 'documents/Week3/'
-    else
-        docfile.upload_to = 'documents/Week4/'"""
+    docfile = models.FileField(upload_to=choices_location)
