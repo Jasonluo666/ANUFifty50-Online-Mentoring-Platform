7032eddc207dfd6782b4982d1e83edc974ddbdbf
Nikita Bhatia <nikitabhatia.17@gmail.com>
2017-07-29 15:02:07 +1000

    Revert "Newer Homepage Version"
    This reverts commit c45d9b8ce0d06030da616d01b7e8f849ff2d1a60.

--- project/fifty_fifty/content/views.py
+++ project/fifty_fifty/content/views.py
@@ -2,15 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.utils import timezone
 from .models import Post, Mentor, Mentee
-from django.http import HttpResponse
 
 class PostList(ListView):
     queryset = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     model = Post
     template_name = 'profile.html'
-
-
-class MenteeList(ListView):
-    model = Mentee
-    template_name = '../webcore/templates/content.html'
-
