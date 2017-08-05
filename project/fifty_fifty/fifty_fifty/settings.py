2527faeca43b3349293b6a1a345c36aa826f63c8
tyrus <tyrus.caldeira@yahoo.com>
2017-08-05 11:33:06 +1000

    Deleted unwanted static files and edited out public static folder

--- project/fifty_fifty/fifty_fifty/settings.py
+++ project/fifty_fifty/fifty_fifty/settings.py
@@ -134,7 +134,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
@@ -142,7 +141,7 @@ STATIC_URL = '/static/'
 
 if DEBUG:
     MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"public")
     MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static","media")
     STATICFILES_DIRS = [
     os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
