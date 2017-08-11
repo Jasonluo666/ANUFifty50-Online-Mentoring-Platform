54a17f6dd8b2fc44529c53a2ac43f0817a876ff9
Nick-Kumar <nikhilanmolkumar@gmail.com>
2017-08-11 11:35:49 +1000

    Changed Admin, webcore_profile

--- project/fifty_fifty/webcore/admin.py
+++ project/fifty_fifty/webcore/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 # Register your models here.
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["uniId","role","user","gender","degree_programme","degree_major"]
+
+admin.site.register(Profile, ProfileAdmin)
